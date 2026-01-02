6f38f4eb616be38de425544670c5758e0efb5725
akhilkadambala3-star <akhilkadambala3@gmail.com>
2026-01-02 05:44:43 +0000

    comparsion in js

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);basic maths lagega
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);these ops are not used as they are not ideal
+
+ // === is used to check strictly,it not only check number but it also check the data type and give output
+
+
+// console.log("2" === 2);output will be false as string!=int,if you use == then output will be true
