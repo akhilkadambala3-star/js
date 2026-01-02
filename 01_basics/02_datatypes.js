771d9146dabb15ce6a161bcbd61be92820e23d22
akhilkadambala3-star <akhilkadambala3@gmail.com>
2026-01-02 04:33:01 +0000

    data types in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version,phele old ver hota tha,ab new ver hai issi liye
+
+// alert( 3 + 3) // we are using nodejs, not browser,browser me 6 output aa jayega yha ni
+
+console.log(3 
+    +
+     3) // code readability should be high issi liye ni likte yese,vrna run toh ye bhi hoga
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 (ye range hai)
+// bigint (use to store big integers like in stock market etc)
+// string => ""
+// boolean => true/false
+// null => standalone value(used when you have to assign null value)
+// undefined => (jab tum declare krdo but usko koi value assign nah kro(eg:state))
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined (this is output)
+console.log(typeof null); // object
